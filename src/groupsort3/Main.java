bef8bc710a6ab09f43fce2d058da806411f4084f
073903999 <073903999@109R030573YCQ22.EDU.YRDSB.NET>
2017-12-05 12:07:54 -0500

    finsihed

--- src/groupsort3/Main.java
+++ src/groupsort3/Main.java
@@ -15,16 +15,17 @@ public class Main {
     
     public static void main(String[] args) {
         Comparable arry[] = {3, 2, 5, 6,4,3,6,5,7,9,7,6,32,2,3,4,1,31,3,434,1, 4, 7, 4, 6};
+        Comparable arr[] = {1,2,3,4,5,6,8};
         
-        
-        insertion(0, arry);
-        for (int i = 0; i < arry.length; i++) {
-            System.out.println(arry[i]);
+        insertion(arry);
+        for (Comparable arry1 : arry) {
+            System.out.println(arry1);
         }
         
     }
 
-    public static void bubble(Comparable[] a) {
+    public static void bubble(Comparable[] a, int b) {
+        b =+ 1;
         boolean swapped;
         Comparable temp;
         do {
@@ -43,7 +44,9 @@ public class Main {
             }
         } while (swapped);
 
-    } 
+    }
+    
+    
     public void selection(Comparable[] array){
         Comparable lowest = 0;
         for (int i = 0; i<array.length;i++){
@@ -62,26 +65,24 @@ public class Main {
     }
     
     // ashers
-    public static void insertion(int val, Comparable[] array){
-        int nexPos = array.length-2;
-        Comparable[] newArray = new Comparable[array.length+1];
+    public static void insertion( Comparable[] arr){
         
-        for(int i = 0; i>=array.length;i++){
-            newArray[i] = array[i];
-        }
-        
-        for(int i = nexPos; i==0;i--){
+        for(int i = arr.length-1; i>=1;i--){
             
-            if(nexPos>val){
-                newArray[i+1] =  newArray[i];
+            Comparable pos = i;
+            Comparable nexpos = i-1;
+            
+            if(( nexpos.compareTo(pos) ) <0){
+                arr[i] =  nexpos;
+                arr[i-1] =  pos;
+            }
+            else if(( arr[i-1].compareTo(arr[i]) ) >0){
+                arr[i] =  pos;
+                arr[i-1] =  nexpos;
             }
             
-            else if(nexPos<=val){
-                newArray[i] = val;
-                break;
-            }
         }
         
-        array = newArray;
     } 
+    
 }
